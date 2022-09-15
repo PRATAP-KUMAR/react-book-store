349297f60cc6905083a016ca9b0dd28a6c9323d6
PRATAP-KUMAR <pratap@fastmail.fm>
2022-09-16 00:25:46 +0530

    ADD CHECK STATUS BUTTON - CATEGORIES COMPONENT

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,10 @@
 import Header from './Header';
+import '../css/Categories.css';
 
 const Categories = () => (
   <>
     <Header />
+    <button type="button" className="CheckButton" onClick={() => alert('Clicked')}>Check Status</button>
   </>
 );
 
